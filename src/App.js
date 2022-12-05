742e6cd23fec5def8d4f12a9087d5163f695e60e
KarLOL82 <klinfeldt@gmail.com>
2022-12-04 21:27:18 -0800

    rendering new todo list items, need to clear input field on submit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export default function App() {
-  const todos = [
-    {id: 1, text: "Wash dishes", done: false},
-    {id: 2, text: "Do laundry", done: false},
-    {id: 3, text: "Take shower", done: false},
-    
-  ]; 
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false },
+  ]);
   return (
     <div className="App">
       <h1>Todo list</h1>
 
       <TodoList todos={todos} />
+      <AddTodo setTodos={setTodos}/>
+
     </div>
   );
 }
 
-function TodoList(props) {
+function TodoList({ todos }) {
   return (
     <ul>
-      {props.todos.map(todo => (
-        <li>{todo.text}</li>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
       ))}
     </ul>
-  )
+  );
+}
+
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false,
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    })
+    inputRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
