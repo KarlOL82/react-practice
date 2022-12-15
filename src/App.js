92b526c45056d2aea468b52ec36a3bd832dcd671
KarLOL82 <klinfeldt@gmail.com>
2022-12-14 20:57:02 -0800

    delete button not displaying

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function DeleteTodo({ todo, setTodos }) {
 
   return (
     <span
-      className="flex text-red-600 font-bold m-left-10 cursor: cursor-pointer"
+      className="flex btn btn-red text-red-600 font-bold m-left-10 cursor: cursor-pointer"
       onClick={handleDeleteTodo}
       role="button"
       // style={{
@@ -104,7 +104,7 @@ function AddTodo({ setTodos }) {
     <form onSubmit={handleAddTodo}>
 
       <input className="border-solid" name="addTodo" placeholder="Add todo" ref={inputRef} />
-      <button className="border-solid" type="submit">Submit</button>
+      <button className="btn btn-blue border-solid" type="submit">Submit</button>
     </form>
   );
 }
