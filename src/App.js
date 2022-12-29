f54db60f246383a83031464e3c8d029c90091e41
KarLOL82 <klinfeldt@gmail.com>
2022-12-28 19:33:30 -0800

    fixed errors

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default function App() {
     { id: 4, text: "Resolve issue", done: false },
     { id: 5, text: "Debug placeholder image issue in Wilco", done: false },
     { id: 6, text: "Render top seller icon in Wilco assignment", done: false},
-    { id: 7, text: "debug Wilco React component", done: false }
+    { id: 7, text: "debug Wilco React component", done: false },
+    { id: 8, text: "Find a new project", done: false}
   ]);
   return (
     <div className="App bg-blue-200">
