a37a2d59b08f218299e36de8ed4c5973db5fd60a
KarLOL82 <klinfeldt@gmail.com>
2022-12-29 20:09:22 -0800

    changing empty list message

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function TodoList({ todos, setTodos }) {
   }
   //Display this text if all items are removed from list
   if (!todos.length) {
-    return <p>No todos left!</p>;
+    return <p>Nothing left to do!</p>;
   }
 
   return (
