cfdf47e9384db6b0f503b47e1b825e834892024a
KarLOL82 <klinfeldt@gmail.com>
2023-01-02 20:04:19 -0800

    adding to seeded list items

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ export default function App() {
     { id: 6, text: "Render top seller icon in Wilco assignment", done: false},
     { id: 7, text: "debug Wilco React component", done: false },
     { id: 8, text: "Find a new project", done: false},
-    { id: 9, text: "Look into AWS credits", done: false }
+    { id: 9, text: "Look into AWS credits", done: false },
+    { id: 10, text: "Make delete button better", done: false }
   ]);
   return (
     <div className="App bg-blue-300">
